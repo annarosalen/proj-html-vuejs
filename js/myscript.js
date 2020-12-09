1d9406918f21d109adfb0b087d49c99ab7b88ba5
Anna <rosalenanna@gmail.com>
2020-12-09 17:04:33 +0100

    vue header

---
+++ js/myscript.js
@@ -0,0 +1,36 @@
+var navbar= new Vue({
+  el: "#navbar-header",
+  data: {
+    search: "",
+    cart: "#",
+    login: "#",
+    logo: "img/dark-logo.png",
+    menu: [
+      {
+        text: "Home",
+        link: "#"
+      },
+      {
+        text: "Pages",
+        link: "#"
+      },
+      {
+        text: "Courses",
+        link: "#"
+      },
+      {
+        text: "Features",
+        link: "#"
+      },
+      {
+        text: "Blog",
+        link: "#"
+      },
+      {
+        text: "Shop",
+        link: "#"
+      }
+    ]
+  }
+
+})
